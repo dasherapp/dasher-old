1f659709505d189f9ec69fd70fab3e93687232e1
Cole Bemis <colebemis@gmail.com>
2017-12-23 01:33:16 -0800

    Add auth header

--- src/components/Apollo.js
+++ src/components/Apollo.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-client-preset';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { GRAPHCOOL_TOKEN_KEY } from '../constants';
+
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cjbhufyy601sx0170z4w9rxw8',
+});
+
+const middlewareLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GRAPHCOOL_TOKEN_KEY);
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  });
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.graph.cool/simple/v1/cjbhufyy601sx0170z4w9rxw8',
-  }),
+  link: middlewareLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
