4806245bfe20c6b2824b02e65df7a96d205a26f4
Cole Bemis <colebemis@gmail.com>
2017-12-24 20:45:17 -0800

    Add AccountMenu to BoardPage

--- src/components/BoardPage.js
+++ src/components/BoardPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import AccountMenu from './AccountMenu';
 
 const BoardPage = ({ match }) => (
   <div>
+    <AccountMenu />
     <h1>Board</h1>
     <p>Id: {match.params.id}</p>
   </div>
