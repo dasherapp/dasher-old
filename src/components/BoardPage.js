8189b86ab50273a67cf71b36718fcb70a40e4bd4
Cole Bemis <colebemis@gmail.com>
2017-12-25 15:15:46 -0800

    Handle incorrect board ids

--- src/components/BoardPage.js
+++ src/components/BoardPage.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import NotFoundPage from './NotFoundPage';
 import AccountMenu from './AccountMenu';
 
-const BoardPage = ({ match, data }) => (
-  <div>
-    <Link to="/">Back</Link>
-    <AccountMenu />
-    {data.loading ? (
-      <div>Loading</div>
-    ) : (
+const BoardPage = ({ match, data }) => {
+  if (data.loading) {
+    return <div>Loading</div>;
+  }
+
+  if (!data.Board) {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <div>
+      <AccountMenu />
+      <Link to="/">Back</Link>
       <div>
         <h1>{data.Board.name}</h1>
         <p>id: {data.Board.id}</p>
         <p>createdAt: {data.Board.createdAt}</p>
         <p>updatedAt: {data.Board.updatedAt}</p>
       </div>
-    )}
-  </div>
-);
+    </div>
+  );
+};
 
 const BOARD = gql`
   query Board($id: ID!) {
