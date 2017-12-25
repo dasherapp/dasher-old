351ec80a19afde03d92f7fe2d512f7bf29c3629d
Cole Bemis <colebemis@gmail.com>
2017-12-24 20:17:11 -0800

    Use arrow function

--- src/components/BoardPage.js
+++ src/components/BoardPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function BoardPage({ match }) {
-  return (
-    <div>
-      <h1>Board</h1>
-      <p>Id: {match.params.id}</p>
-    </div>
-  );
-}
+const BoardPage = ({ match }) => (
+  <div>
+    <h1>Board</h1>
+    <p>Id: {match.params.id}</p>
+  </div>
+);
 
 export default BoardPage;
