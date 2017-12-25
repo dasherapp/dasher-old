cc103119eeee0d6d95a20f93ab1aab4d0c7b3bdd
Cole Bemis <colebemis@gmail.com>
2017-12-25 02:35:50 -0800

    Display Board info on BoardPage

--- src/components/BoardPage.js
+++ src/components/BoardPage.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import AccountMenu from './AccountMenu';
 
-const BoardPage = ({ match }) => (
+const BoardPage = ({ match, data }) => (
   <div>
+    <Link to="/">Back</Link>
     <AccountMenu />
-    <h1>Board</h1>
-    <p>Id: {match.params.id}</p>
+    {data.loading ? (
+      <div>Loading</div>
+    ) : (
+      <div>
+        <h1>{data.Board.name}</h1>
+        <p>id: {data.Board.id}</p>
+        <p>createdAt: {data.Board.createdAt}</p>
+        <p>updatedAt: {data.Board.updatedAt}</p>
+      </div>
+    )}
   </div>
 );
 
-export default BoardPage;
+const BOARD = gql`
+  query Board($id: ID!) {
+    Board(id: $id) {
+      id
+      name
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export default graphql(BOARD, {
+  options: ({ match }) => ({ variables: { id: match.params.id } }),
+})(BoardPage);
