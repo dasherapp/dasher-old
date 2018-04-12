401f74ea8ad3ab812040822d43fe037c1c2f6c97
Cole Bemis <colebemis@gmail.com>
2018-04-11 21:19:36 -0700

    Simplify ModalRoot return

--- src/components/ModalRoot.js
+++ src/components/ModalRoot.js
@@ -8,9 +8,7 @@ const ModalRoot = () => (
     {modal => {
       const { component: Component, props } = modal.state
 
-      return Component ? (
-        <Component hideModal={modal.hideModal} {...props} />
-      ) : null
+      return Component && <Component hideModal={modal.hideModal} {...props} />
     }}
   </Subscribe>
 )
